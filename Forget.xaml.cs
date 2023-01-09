817aeae7317825b623d8f7f82a246a7ce77ed38f
Shreya.Mehta <Shreya.Mehta@GEMGN-220436>
2023-01-09 13:01:55 +0530

    Add project files.

---
+++ Forget.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace WpfApp2
+{
+    public partial class Forget : Window
+    {
+        public Forget()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_click(object sender, RoutedEventArgs args)
+        {
+            SqlConnection sqlcon = new SqlConnection("Server=10.50.18.16; Database=training_db; User Id=SVC_TRANING; Password=Gemini@123; TrustServerCertificate=True");
+            try
+            {
+               
+                sqlcon.Open();
+                String query = "update [dbo].[User1] set password=@pass where email=@email";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlcon);
+                sqlCmd.Parameters.AddWithValue("@email", email.Text);
+                sqlCmd.Parameters.AddWithValue("@pass", pass.Password);
+
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Password changed successfully");
+                LoginScreen loginScreen = new LoginScreen();
+                loginScreen.Show();
+
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+            finally
+            {
+                sqlcon.Close();
+            }
+
+        }
+
+    }
+}
